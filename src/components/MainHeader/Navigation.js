73febb22ef28eb4530d60c68ec100781da0fb38a
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-10-16 11:10:49 +0530

    Rules of hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,8 +1,7 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 
-
+import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
-import AuthContext from '../../store/AuthContext';
 
 const Navigation = () => {
   const ctx = useContext(AuthContext);
